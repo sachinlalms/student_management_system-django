20f5cc4e36332c2c099adac8fe7310b53e973146
sachinlalms <sachinlalms95@gmail.com>
2020-07-10 14:17:57 +0530

    login-super

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -1,7 +1,40 @@
+from django.contrib.auth import login, logout
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate
 
 # Create your views here.
+from student_management_app.EmailBackEnd import EmailBackEnd
+
+
 def showDemoPage(request):
     return render(request, "demo.html")
+
+
 def ShowLoginPage(request):
-    return render(request,"login.html")
+    return render(request, "login.html")
+
+
+def doLogin(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+
+        user = EmailBackEnd.authenticate(request, username=request.POST.get("email"),password=request.POST.get("password"))
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Email : " + request.POST.get("email") + " Password : " + request.POST.get("password"))
+        else:
+            return HttpResponse("Invalid id login")
+
+
+def GetUserDetails(request):
+    if request.user is not None:
+        return HttpResponse("User : " + request.user.email + " Usertype :" + request.user.user_type)
+    else:
+        return HttpResponse("Please login First!!")
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect("/")
--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -126,3 +126,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = "student_management_app.CustomUser"
+AUTHENTICATION_BACKENDS = ['student_management_app.EmailBackEnd.EmailBackEnd']
